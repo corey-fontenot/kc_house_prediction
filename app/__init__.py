a0be8278a1429c4664e07b8d5bfedcf79feb20f9
Corey Fontenot <corey.fontenot@gmail.com>
2020-08-14 02:58:17 -0700

    Add Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
-# creating instance of Flask
+# import flask-bootstrap
+from flask_bootstrap import Bootstrap
+
+# Initializations
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 # importing routes at bottom of file to avoid circular imports
 from app import routes
