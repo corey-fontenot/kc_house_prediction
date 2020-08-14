c61830e7ca2db799aad0f0ed6457d8964b39c5ca
Corey Fontenot <corey.fontenot@gmail.com>
2020-08-14 05:01:03 -0700

    Add configuration object

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 
 
 # Initializations
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # importing routes at bottom of file to avoid circular imports
 from app import routes
