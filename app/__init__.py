31df4c74e1c45e1299cbcff5b20a491fb185507b
Corey Fontenot <corey.fontenot@gmail.com>
2020-08-14 04:11:12 -0700

    Add render_template

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 
-# import flask-bootstrap
-from flask_bootstrap import Bootstrap
 
 # Initializations
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 # importing routes at bottom of file to avoid circular imports
 from app import routes
