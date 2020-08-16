89d3204b13938ddf5579295aa260c381b49ad78a
Corey Fontenot <corey.fontenot@gmail.com>
2020-08-15 23:27:52 -0700

    Add comments

--- app/routes.py
+++ app/routes.py
@@ -21,16 +21,24 @@ def dashboard():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # if user is already logged in they are redirected to the dashboard page
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     form = LoginForm()
+
+    # if form data is valid and username/password are correct the user is logged in
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user)
+
+        # sets page to load after being logged in
         next_page = request.args.get('next')
+
+        # Sets page to dashboard if a page is not specified or url is not relative
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('dashboard')
         flash('You have successfully logged in')
@@ -47,6 +55,7 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
+# logs user out then redirects to the login page
 @app.route('/logout')
 def logout():
     logout_user()
